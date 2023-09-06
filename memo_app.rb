2d1daa8374f0c30702806424bacead7ea5276391
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-09-06 10:55:55 +0900

    ルーティングを変更

--- memo_app.rb
+++ memo_app.rb
@@ -38,7 +38,7 @@ helpers do
   end
 end
 
-get '/memo-top' do
+get '/' do
   all_memos = []
   Memo.open_json.each do |memo|
     all_memos << Memo.new(memo[:memo_id], memo[:title], memo[:content])
@@ -55,7 +55,7 @@ get '/memos/new' do
 end
 
 post '/memos/new' do
-  unless params[:title] == ''
+  if params[:title] != ''
     new_memo = Memo.new(SecureRandom.uuid, escape(params[:title]), escape(params[:content]))
     new_memo_to_list = {}
     new_memo_to_list[:memo_id] = new_memo.memo_id
@@ -66,7 +66,7 @@ post '/memos/new' do
     Memo.updata_json(all_memos)
   end
 
-  redirect '/memo-top'
+  redirect '/'
 end
 
 get '/memos/:id' do
@@ -83,7 +83,7 @@ delete '/memos/del' do
   all_memos.delete_if { |memo| memo[:memo_id] == params[:id] }
   Memo.updata_json(all_memos)
 
-  redirect '/memo-top'
+  redirect '/'
 end
 
 get '/memos/:id/edit' do
@@ -99,9 +99,9 @@ patch '/memos/:id' do
   all_memos[index_num][:content] = escape(params[:content])
   Memo.updata_json(all_memos)
 
-  redirect '/memo-top'
+  redirect '/'
 end
 
 not_found do
-  '指定されたページは存在しません。/memo-appにアクセスしてください。'
+  '指定されたページは存在しません。<a href="/">トップページ</a>にアクセスしてください。'
 end
